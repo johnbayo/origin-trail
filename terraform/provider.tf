adc38799bccb6cd123ee82ef958550ac13ed8e1b
johnbayo <john.bayo@web.de>
2022-08-01 16:27:00 +0200

    updated backend configuration for aws

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,9 +5,19 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "second-interview"
+    key            = "dev/terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
   }
 }
-
 
 provider "digitalocean" {
   token = var.do_token
